837c94485139c17aa65cbfa68465a73874ae9b7a
statios <statios@kakao.com>
2021-02-02 13:53:13 +0900

    Update random dice number

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,14 +16,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(child: Image.asset('images/dice1.png')),
-        Expanded(child: Image.asset('images/dice1.png')),
-      ],
+    int leftDiceNumber = Random().nextInt(6) + 1;
+    int rightDiceNumber = Random().nextInt(6) + 1;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  // leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  // rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
